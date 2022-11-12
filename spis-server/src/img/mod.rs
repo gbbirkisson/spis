679732614cd8ef2a2f10bdf735b806309cfa6a38
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2022-11-13 00:16:30 +0100

    Do some cleanup

--- spis-server/src/img/mod.rs
+++ spis-server/src/img/mod.rs
@@ -1,5 +1,7 @@
 use chrono::prelude::*;
+use eyre::{eyre, Result};
 use rayon::prelude::*;
+use sqlx::{Pool, Sqlite};
 use std::path::Path;
 use std::time::UNIX_EPOCH;
 use std::{
@@ -9,8 +11,10 @@ use std::{
 };
 use uuid::{Builder, Uuid};
 
-use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 use walkdir::WalkDir;
+
+use crate::db;
 
 static IMAGE_FORMAT: &[&str] = &[".jpg"];
 static THUMBNAIL_FORMAT: &str = "webp";
@@ -81,32 +85,85 @@ pub fn image_thumb(thumb_dir: &Path, uuid: &Uuid) -> PathBuf {
     res
 }
 
-pub fn image_processor(img_dir: PathBuf, thumb_dir: PathBuf, tx: Sender<ProcessedImage>) {
+pub async fn process(pool: Pool<Sqlite>, img_dir: PathBuf, thumb_dir: PathBuf) {
     tracing::info!("Starting image processing");
-    tokio::task::spawn_blocking(move || {
-        do_walk(img_dir, thumb_dir, tx);
-    });
+    let (tx, mut rx) = channel(32);
+    let mut done_recv = image_processor(img_dir, thumb_dir, tx);
+    let processor_pool = pool.clone();
+
+    loop {
+        tokio::select! {
+            done = done_recv.recv() => {
+                match done {
+                    Some(count) => {
+                        tracing::info!("Processed {} images", count);
+                        break;
+                    },
+                    None => {
+                        tracing::debug!("None from done channel");
+                    }
+                }
+            }
+            img = rx.recv() => {
+                match img {
+                    Some(img) => {
+                        db::insert_image(&processor_pool, img).await;
+                    }
+                    None => {
+                        tracing::debug!("None from channel");
+                    }
+                }
+
+            }
+        }
+    }
 }
 
-fn do_walk(img_dir: PathBuf, thumb_dir: PathBuf, tx: Sender<ProcessedImage>) {
+fn image_processor(
+    img_dir: PathBuf,
+    thumb_dir: PathBuf,
+    image_sender: Sender<ProcessedImage>,
+) -> Receiver<usize> {
+    let (done_send, done_recv) = channel(1);
+    tokio::task::spawn_blocking(move || {
+        do_walk(img_dir, thumb_dir, image_sender, done_send);
+    });
+    done_recv
+}
+
+fn do_walk(
+    img_dir: PathBuf,
+    thumb_dir: PathBuf,
+    tx: Sender<ProcessedImage>,
+    done_send: Sender<usize>,
+) {
     let walk: Vec<_> = WalkDir::new(img_dir)
         .into_iter()
         .filter_map(|r| r.ok())
         .filter(|e| e.has_ext(IMAGE_FORMAT))
         .par_bridge()
-        .map(|e| do_process(thumb_dir.clone(), tx.clone(), e).ok())
+        .map(|e| {
+            if let Err(err) = do_process(thumb_dir.clone(), tx.clone(), &e) {
+                let path = e.path().to_str().unwrap();
+                tracing::error!("Failed processing image {path}: {err}")
+            }
+        })
         .collect();
-    tracing::info!("Successfully processed {} images", walk.len());
+
+    if let Err(e) = done_send
+        .blocking_send(walk.len())
+        .map_err(|e| eyre!("Failed sending done to channel: {:?}", e.to_string()))
+    {
+        tracing::error!("{e}")
+    }
 }
 
 fn do_process(
     thumb_dir: PathBuf,
     tx: Sender<ProcessedImage>,
-    image_entry: walkdir::DirEntry,
-) -> Result<(), ImageProcessError> {
-    let image_bytes = fs::read(image_entry.path()).map_err(|_| ImageProcessError {
-        msg: "failed to read file".to_string(),
-    })?;
+    image_entry: &walkdir::DirEntry,
+) -> Result<()> {
+    let image_bytes = fs::read(image_entry.path())?;
     let image_hash = md5::compute(&image_bytes);
     let image_uuid = *Builder::from_md5_bytes(image_hash.into()).as_uuid();
 
@@ -116,19 +173,16 @@ fn do_process(
 
     if !image_thumbnail_path.exists() {
         tracing::info!("Creating thumbnail: {:?}", image_thumbnail_path);
-        let mut image = image::open(image_entry.path()).expect("should work");
+        let mut image = image::open(image_entry.path())?;
         image = image.thumbnail(THUMBNAIL_HEIGHT * 2, THUMBNAIL_HEIGHT);
-
-        image
-            .save(image_thumbnail_path)
-            .expect("Saving image should work"); // TODO
+        image.save(image_thumbnail_path)?;
 
         // TODO: load image exif data
 
-        let image_meta = image_entry.metadata().expect("Able to get metadata"); // TODO
+        let image_meta = image_entry.metadata()?;
         let data = ProcessedImageData {
-            created_at: image_meta.created().expect("To work").conv(),
-            modified_at: image_meta.modified().expect("To work").conv(),
+            created_at: image_meta.created()?.conv(),
+            modified_at: image_meta.modified()?.conv(),
             exif: None,
         };
 
@@ -140,10 +194,10 @@ fn do_process(
         image: image_entry.path().to_path_buf(),
         data: image_data,
     };
+
     tracing::debug!("Sending image to channel {:?}", image.image);
-    if tx.blocking_send(image).is_err() {
-        tracing::error!("Failed sending image to channel");
-    }
+    tx.blocking_send(image)
+        .map_err(|e| eyre!("Failed sending image to channel: {:?}", e.to_string()))?;
 
     Ok(())
 }
--- spis-server/src/main.rs
+++ spis-server/src/main.rs
@@ -1,35 +1,32 @@
 use std::{net::TcpListener, path::PathBuf};
 
 use spis_server::{db, img, server};
-use tokio::sync::mpsc::channel;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    tracing_subscriber::fmt::init();
+    // Setup logging
+    dotenv::dotenv().ok();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
+    // Setup paths
     let img_dir = PathBuf::from("dev/api/images");
     let thumb_dir = PathBuf::from("dev/api/state/thumbnails");
     let db_url = PathBuf::from("dev/api/state/spis.db");
 
+    // Setup DB
     let pool = db::setup_db(db_url).await.unwrap();
 
-    let (tx, mut rx) = channel(32);
-    img::image_processor(img_dir, thumb_dir, tx);
+    // Process images
     let processor_pool = pool.clone();
-
     tokio::spawn(async move {
-        loop {
-            match rx.recv().await {
-                Some(img) => {
-                    db::insert_image(&processor_pool, img).await;
-                }
-                None => {
-                    tracing::info!("None from channel");
-                }
-            }
-        }
+        img::process(processor_pool, img_dir, thumb_dir).await;
     });
 
+    // Start server
     let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed to bind random port");
     let server = server::run(listener, pool).expect("Failed to create server");
     server.await
