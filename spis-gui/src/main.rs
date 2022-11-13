7d8ecfb68a142fc2f7dfad7296142e5976088b8f
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2022-11-13 17:46:18 +0100

    More button:w

--- spis-gui/src/main.rs
+++ spis-gui/src/main.rs
@@ -1,8 +1,8 @@
 use reqwasm::http::Request;
 use spis_model::Image;
+use sycamore::futures::spawn_local_scoped;
 use sycamore::prelude::*;
 use sycamore::suspense::Suspense;
-use wasm_bindgen_futures::spawn_local;
 
 const PAGE_SIZE: usize = 10;
 
@@ -23,19 +23,20 @@ fn render_image<G: Html>(cx: Scope<'_>, image: Image) -> View<G> {
 #[component]
 async fn Images<G: Html>(cx: Scope<'_>) -> View<G> {
     let images = use_context::<RcSignal<Vec<Image>>>(cx);
-    let image_set = images.clone();
 
-    spawn_local(async move {
-        let old_images = image_set.get();
-        let mut new_images: Vec<Image> = Vec::with_capacity(old_images.len() + PAGE_SIZE);
+    let load_more = move |_| {
+        spawn_local_scoped(cx, async move {
+            let old_images = images.get();
+            let mut new_images: Vec<Image> = Vec::with_capacity(old_images.len() + PAGE_SIZE);
 
-        for image in old_images.iter() {
-            new_images.push(image.clone());
-        }
+            for image in old_images.iter() {
+                new_images.push(image.clone());
+            }
 
-        new_images.append(&mut fetch_images().await.unwrap());
-        image_set.set(new_images);
-    });
+            new_images.append(&mut fetch_images().await.unwrap());
+            images.set(new_images);
+        });
+    };
 
     view! { cx,
         ul(class="image-gallery") {
@@ -44,6 +45,7 @@ async fn Images<G: Html>(cx: Scope<'_>) -> View<G> {
                 view=|cx, image| render_image(cx, image),
             )
         }
+        button(on:click=load_more) { "More" }
     }
 }
 
