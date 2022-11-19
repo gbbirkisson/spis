38423133a18e4af638659db69eaffab9b4691e20
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2022-11-19 12:55:14 +0100

    Add endless scrolling

--- spis-gui/src/main.rs
+++ spis-gui/src/main.rs
@@ -1,16 +1,19 @@
 use chrono::{DateTime, Utc};
 use reqwasm::http::Request;
 use spis_model::Image;
-use sycamore::futures::spawn_local_scoped;
 use sycamore::prelude::*;
 use sycamore::suspense::Suspense;
+use wasm_bindgen::prelude::Closure;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::spawn_local;
 
-const PAGE_SIZE: usize = 100;
+const API_IMAGE_PER_REQ: usize = 30;
+const PAGE_PX_LEFT_TO_FETCH_MORE: f64 = 200.0;
 
 async fn fetch_images(prev: Option<DateTime<Utc>>) -> Result<Vec<Image>, reqwasm::Error> {
     let url = match prev {
-        None => format!("/api/?page_size={}", PAGE_SIZE),
-        Some(prev) => format!("/api/?page_size={}&prev={}", PAGE_SIZE, prev),
+        None => format!("/api/?page_size={}", API_IMAGE_PER_REQ),
+        Some(prev) => format!("/api/?page_size={}&prev={}", API_IMAGE_PER_REQ, prev),
     };
     let res = Request::get(&url).send().await?;
     let body = res.json::<Vec<Image>>().await?;
@@ -27,38 +30,88 @@ fn render_image<G: Html>(cx: Scope<'_>, image: Image) -> View<G> {
 
 #[component]
 async fn Images<G: Html>(cx: Scope<'_>) -> View<G> {
-    let images = use_context::<RcSignal<Vec<Image>>>(cx);
-    let more_button_visible = use_context::<RcSignal<String>>(cx);
+    // Setup signals
+    let images: RcSignal<Vec<Image>> = create_rc_signal(fetch_images(None).await.unwrap());
+    let images_loading = create_rc_signal(false);
+    let images_reached_end = create_rc_signal(false);
+    let images_ref = create_ref(cx, images.clone());
 
-    let load_more = move |_| {
-        spawn_local_scoped(cx, async move {
-            let old_images = images.get();
-            let mut new_images: Vec<Image> = Vec::with_capacity(old_images.len() + PAGE_SIZE);
+    // Create scrolling callback
+    let callback: Closure<dyn FnMut()> = Closure::new(move || {
+        let images = images.clone();
+        let images_loading = images_loading.clone();
+        let images_reached_end = images_reached_end.clone();
 
-            for image in old_images.iter() {
-                new_images.push(image.clone());
+        spawn_local(async move {
+            if images_reached_end.get().as_ref() == &true {
+                return;
             }
 
-            let prev = new_images.last().map(|i| i.created_at);
-            let mut fetched_images = fetch_images(prev).await.unwrap(); // TODO
+            if images_loading.get().as_ref() == &true {
+                return;
+            }
+            images_loading.set(true);
 
-            if fetched_images.len() != PAGE_SIZE {
-                more_button_visible.set("hide".to_string());
+            let window = web_sys::window().expect("Failed to get window");
+            let document = window.document().expect("Failed to get document");
+            let body = document.body().expect("Failed to get body");
+
+            let win_inner_height = window
+                .inner_height()
+                .expect("Failed to get window.innerHeight")
+                .as_f64()
+                .expect("Failed to convert window.innerHeight");
+            let win_page_y_offset = window
+                .page_y_offset()
+                .expect("Failed to get window.pageYOffset");
+            let body_offset_height = f64::from(body.offset_height());
+
+            let near_end_of_page = win_inner_height + win_page_y_offset
+                >= body_offset_height - PAGE_PX_LEFT_TO_FETCH_MORE;
+
+            if near_end_of_page {
+                let old_images = images.get();
+                let mut new_images: Vec<Image> =
+                    Vec::with_capacity(old_images.len() + API_IMAGE_PER_REQ);
+
+                for image in old_images.iter() {
+                    new_images.push(image.clone());
+                }
+
+                let prev = new_images.last().map(|i| i.created_at);
+                let mut fetched_images = fetch_images(prev).await.unwrap(); // TODO
+
+                if fetched_images.len() != API_IMAGE_PER_REQ {
+                    images_reached_end.set(true);
+                }
+
+                new_images.append(&mut fetched_images);
+                images.set(new_images);
             }
 
-            new_images.append(&mut fetched_images);
-            images.set(new_images);
+            images_loading.set(false);
         });
-    };
+    });
 
+    // Setup callback
+    let window = web_sys::window().expect("Failed to get window");
+    window
+        .add_event_listener_with_callback_and_bool(
+            "scroll",
+            callback.as_ref().unchecked_ref(),
+            false,
+        )
+        .expect("Failed to set listener");
+    callback.forget();
+
+    // Return view
     view! { cx,
         ul(class="image-gallery") {
             Indexed(
-                iterable=images,
+                iterable=images_ref,
                 view=|cx, image| render_image(cx, image),
             )
         }
-        button(class=more_button_visible, on:click=load_more) { "More" }
     }
 }
 
