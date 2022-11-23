4fa602b636544f1afb650c98d8633c5e4f70a09f
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2022-11-23 10:33:03 +0100

    GH fix

--- docker/pick-release.sh
+++ docker/pick-release.sh
@@ -4,7 +4,7 @@ set -eux
 
 if uname -m | grep x86_64; then
     TARGET=x86_64-unknown-linux-gnu
-elif uname -m | grep aarch64 ; then
+elif uname -m | grep armv7l ; then
     TARGET=armv7-unknown-linux-gnueabihf
 else
     echo "No binary for architecture $(uname -m)"
