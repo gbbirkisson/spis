a22f438b03ccbc55fc52aabdf761a23acd9a2e9e
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2022-11-23 15:22:52 +0100

    Add more arch

--- docker/pick-release.sh
+++ docker/pick-release.sh
@@ -6,6 +6,8 @@ if uname -m | grep x86_64; then
     TARGET=x86_64-unknown-linux-gnu
 elif uname -m | grep armv7l ; then
     TARGET=armv7-unknown-linux-gnueabihf
+elif uname -m | grep aarch64 ; then
+    TARGET=aarch64-unknown-linux-gnu
 else
     echo "No binary for architecture $(uname -m)"
     exit 1
