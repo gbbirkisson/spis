0af814a25056019ce9666b2b42ea24b63ae8472c
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-07-04 21:36:38 +0200

    More tests

--- make/Makefile-setup.mk
+++ make/Makefile-setup.mk
@@ -2,6 +2,7 @@
 setup-toolchain: ## Setup rust toolchain
 	$(info $(M) Setup rust toolchain)
 	$(Q) rustup show
+	$(Q) rustup target add wasm32-unknown-unknown
 	$(Q) cat rust-toolchain.toml | grep '# bin' | xargs -n 4 sh -c 'cargo install --version $$3 $$2'
 
 .PHONY: setup
