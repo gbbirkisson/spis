673a9545125fcdc01009f40b4cd0eff7842d0cf1
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-07-04 23:07:21 +0200

    Overhaul github actions (#97)

--- make/Makefile-ci.mk
+++ make/Makefile-ci.mk
@@ -1,29 +1,27 @@
 .PHONY: fmt
 fmt: ## Run format check
-	$(info $(M) Run formatter)
+	$(Q) echo "$(M) Run formatter"
 	$(Q) cargo fmt -- --check
-	$(info $(M) Formatter done!)
+	$(Q) echo "$(M) Formatter done!"
 
 .PHONY: lint
 lint: ${DEV_DB_FILE} ## Run lint check
-	$(info $(M) Run linter)
+	$(Q) echo "$(M) Run linter"
 	$(Q) cargo clippy -- -D warnings
-	$(info $(M) Linter done!)
+	$(Q) echo "$(M) Run Linter done!"
 
 .PHONY: test
 test: ${DEV_DB_FILE} ## Run tests with coverage report
-	$(info $(M) Run tests)
-	$(Q) cargo tarpaulin --version > /dev/null || cargo install cargo-tarpaulin
+	$(Q) echo "$(M) Run tests"
 	$(Q) cargo tarpaulin --ignore-tests --workspace --timeout 120 --skip-clean --out Xml
-	$(info $(M) Tests passed!)
+	$(Q) echo "$(M) Tests passed!"
 
 .PHONY: audit
 audit: ## Run audit on dependencies
-	$(info $(M) Run audit)
-	$(Q) cargo audit --version > /dev/null || cargo install cargo-audit
+	$(Q) echo "$(M) Run audit"
 	$(Q) cargo audit
-	$(info $(M) Audit passed!)
+	$(Q) echo "$(M) Audit passed!"
 
 .PHONY: ci
 ci: fmt lint audit test ## Run all CI steps
-	$(info $(M) CI done!)
+	$(Q) echo "$(M) CI done!"
--- make/Makefile-setup.mk
+++ make/Makefile-setup.mk
@@ -1,21 +1,22 @@
-.PHONY: setup
-setup: ${DEV_MEDIA_DIR} ${DEV_DB_FILE} ## Setup project dependencies and dirs
-	$(info $(M) Installing cargo binaries)
-	$(Q) cargo install watchexec-cli
-	$(Q) cargo install trunk
-
-	$(info $(M) Add rust components/targets)
-	$(Q) rustup component add rustfmt
-	$(Q) rustup component add clippy
+.PHONY: setup-toolchain
+setup-toolchain: ## Setup rust toolchain
+	$(info $(M) Setup rust toolchain)
+	$(Q) rustup show
 	$(Q) rustup target add wasm32-unknown-unknown
+	$(Q) cat rust-toolchain.toml | grep '# bin' | xargs -n 4 sh -c 'cargo install --version $$3 $$2'
 
-	$(info $(M) Install apt packages)
+.PHONY: setup
+setup: ${DEV_MEDIA_DIR} ${DEV_DB_FILE} setup-toolchain ## Setup project dependencies and dirs
+	$(Q) echo "$(M) Installing cargo binaries"
+	$(Q) cargo install watchexec-cli
+
+	$(Q) echo "$(M) Install apt packages"
 	$(Q) sudo apt install -y nginx ffmpeg
 
-	$(info $(M) Setup done!)
+	$(Q) echo "$(M) Setup done!"
 
 .PHONY: scp-img
 scp-img:
 	$(info $(M) Copying images)
 	$(Q) scp -r stufur:media ${DEV_BASE_DIR}
-	$(info $(M) Copy done!)
+	$(Q) echo "$(M) Copy done!"
