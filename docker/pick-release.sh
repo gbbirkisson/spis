2da783c3ee1073557ff45ba6534ab40e1dc79111
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2024-02-14 09:11:21 +0100

    fix: More build fixes

---
+++ docker/pick-release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -eux
+
+if uname -m | grep x86_64; then
+    TARGET=x86_64-unknown-linux-gnu
+elif uname -m | grep armv7l; then
+    TARGET=armv7-unknown-linux-gnueabihf
+elif uname -m | grep aarch64; then
+    TARGET=aarch64-unknown-linux-gnu
+else
+    echo "No binary for architecture $(uname -m)"
+    exit 1
+fi
+
+cp ${RELEASE_DIR}/spis-${TARGET} ${BIN_LOCATION}
